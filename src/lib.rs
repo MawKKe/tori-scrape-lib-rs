280c97eaf798121df6819073a9766e3e85aac368
Markus Holmström (MawKKe) <markus@mawkke.fi>
2024-01-28 17:02:34 +0200

    Use chrono crate
    Use chrono instead of hand rolling date/time types

--- src/lib.rs
+++ src/lib.rs
@@ -2,21 +2,7 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
-#[derive(Debug, PartialEq)]
-enum Month {
-    January = 1,
-    February = 2,
-    March = 3,
-    April = 4,
-    May = 5,
-    June = 6,
-    July = 7,
-    August = 8,
-    September = 9,
-    October = 10,
-    November = 11,
-    December = 12,
-}
+use chrono::Month;
 
 fn parse_month_short(short_name: &str) -> Result<Month, String> {
     match short_name {
